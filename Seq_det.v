b7b9004eea1313325bcac0c7b70a2e6e96187af6
vbyash <129272123+vbyash@users.noreply.github.com>
2023-06-17 22:02:16 +0530

    Update Seq_det.v

--- Seq_det.v
+++ Seq_det.v
@@ -1,75 +1,106 @@
-module morfsm(din, reset, clk, y);
-output reg y;
-input din;
-input clk;
-input reset;
-reg [2:0] cst, nst;
-parameter S0 = 3'b000,
-          S1 = 3'b001,
-          S2 = 3'b010,
-          S3 = 3'b100,
-            S4 = 3'b101;
-always @(cst or din)
- begin
- case (cst)
-   S0: if (din == 1'b1)
-          begin
-         nst = S1;
-          y=1'b0;
-          end
+`timescale 1ns/1ps
+module Seq_Detect(data_in, clock, reset, detect);
+  input clock;
+  input reset;
+  input wire data_in;
+  output reg detect;
+  
+  reg[2:0] current_state, next_state;
+  parameter S0= 3'b000,
+  			S1= 3'b001,
+    		S2= 3'b010,
+  			S3= 3'b011,  
+  			S4= 3'b100,
+  			S5= 3'b101,
+  			S6= 3'b110;
+  
+  // Combinational logic for next state
+  
+  always @(current_state,data_in)
+    begin
+      case(current_state)
+          S0:begin
+              if(data_in)
+                next_state=S1;
+              else
+                next_state=S0;
+            end
+        
+         S1:begin
+              if(data_in)
+                next_state=S1;
+              else
+                next_state=S2;
+            end
+        
+         S2:begin
+              if(data_in)
+                next_state=S3;
+              else
+                next_state=S0;
+            end
+        
+         S3:begin
+              if(data_in)
+                next_state=S4;
+              else
+                next_state=S2;
+            end
+        
+         S4:begin
+              if(data_in)
+                next_state=S1;
+              else
+                next_state=S5;
+            end
+        
+         S5:begin
+              if(data_in)
+                next_state=S3;
+              else
+                next_state=S6;
+            end
+        
+         S6:begin
+              if(data_in)
+                next_state=S1;
+              else
+                next_state=S0;
+            end
+        
+        default: next_state=S0;
+
+      endcase
+      
+      
+    end
+  
+  
+  //Sequential logic for current state
+  always @(posedge clock)
+    begin
+      if(reset)
+        current_state<=S0;
       else
-          begin
-          nst = cst;
-          y=1'b0;
-          end  
-S1: if (din == 1'b0)
-          begin
-        nst = S2;
-          y=1'b0;
-          end
-       else
-          begin
-          nst = cst;
-          y=1'b0;
-          end
-   S2: if (din == 1'b1)
-          begin
-         nst = S3;
-          y=1'b0;
-          end    
-            else
-          begin
-          nst = S0;
-          y=1'b0;
-          end  
-S3: if (din == 1'b0)
-          begin
-         nst = S4;
-          y=1'b0;
-          end
-       else
-          begin
-          nst = S1;
-          y=1'b0;
-          end
-S4: if (din == 1'b0)
-          begin
-         nst = S0;
-          y=1'b1;
-          end
-          else
-          begin
-          nst = S1;
-          y=1'b1;
-          end
-   default: nst = S0;
-  endcase
-end
-always@(posedge clk)
-begin
- if (reset)
-   cst <= S0;
- else
-   cst <= nst;
-end
+        current_state<=next_state;
+    end
+  
+  //Output logic
+  
+  always @(current_state)
+    begin 
+      case(current_state)
+        S0: detect=1'b0;
+        S1: detect=1'b0;
+        S2: detect=1'b0;
+        S3: detect=1'b0;
+        S4: detect=1'b0;
+        S5: detect=1'b0;
+        S6: detect=1'b1;
+        
+        default: detect=1'b0;
+        
+      endcase
+    end
 endmodule
+  
