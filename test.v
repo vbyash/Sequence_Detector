76ba9c70364d6cc1c32d242e419f20eb3c307176
vbyash <129272123+vbyash@users.noreply.github.com>
2023-06-17 22:01:24 +0530

    Update test.v

--- test.v
+++ test.v
@@ -1,28 +1,50 @@
-module morfsm_tb;
-reg din,clk,reset;
-wire y;
-morfsm m1(din, reset, clk, y);
-initial
-begin
-$dumpfile("dump.vcd");
-$dumpvars;
-reset=0       ;clk=0;din=0;
-$monitor($time, , ,"c=%b",clk,,"y=%b",y,,"r=%b",reset,,"d=%b",din);
-#10 din=1;
-#10 din=1;
-#10 din=1;
-#10 din=0;
-#10 din=1;
-#10 din=0;
-#10 din=1;
-#10 din=0;
-#10 din=1;
-//#5 reset=1;
-//#5 reset=0;
+`timescale 1ns / 1ps
 
-end
-always
-#5 clk=~clk;
-initial
-#100 $finish ;
+module tb_Sequence_Detector_Moore_FSM_Verilog;
+  
+ reg din;
+ reg clk;
+ reg rst;
+ wire out;
+
+ Seq_Detect uut (.data_in(din), .clock(clk), .reset(rst), .detect(out));
+
+  
+ initial begin
+   $dumpfile("dump.vcd");
+   $dumpvars;
+   $monitor($time, , ,"c=%b",clk,,"y=%b",out,,"r=%b",rst,,"d=%b",din);
+ clk = 0;
+ forever #5 clk = ~clk;
+ end 
+
+  
+ initial 
+   begin
+  din = 0;
+  rst = 1;
+ 
+  #10 rst = 0;
+  #10  din = 1;
+  #10  din = 0;
+  #10  din = 1; 
+  #10  din = 1; 
+  #10  din = 0; 
+  #10  din = 0;
+  #10  din = 0; 
+  #10  din = 1; 
+  #10  din = 0; 
+  #10  din = 1;
+  #10  din = 1; 
+  #10  din = 0; 
+  #10  din = 0;
+  #10  din = 1; 
+ end
+      
+  initial
+    begin 
+      #200;
+      $finish;
+    end
+  
 endmodule
