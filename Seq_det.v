95262111a7336daac8aa5b7638f41b09f07fa34e
vbyash <129272123+vbyash@users.noreply.github.com>
2023-06-20 15:01:48 +0530

    Update Seq_det.v

--- Seq_det.v
+++ Seq_det.v
@@ -6,13 +6,14 @@ module Seq_Detect(data_in, clock, reset, detect);
   output reg detect;
   
   reg[2:0] current_state, next_state;
-  parameter S0= 3'b000,
-  			S1= 3'b001,
-    		S2= 3'b010,
-  			S3= 3'b011,  
-  			S4= 3'b100,
-  			S5= 3'b101,
-  			S6= 3'b110;
+  parameter 
+          S0= 3'b000,
+          S1= 3'b001,
+          S2= 3'b010,
+          S3= 3'b011,  
+          S4= 3'b100,
+          S5= 3'b101,
+          S6= 3'b110;
   
   // Combinational logic for next state
   
